49dee968e3b7ff80bd05bec00e298402428e1978
OlegKomissarov <0leg.komissarov@mail.ru>
2019-03-27 18:09:18 +0300

    Add unfavourite and return buttons to favourites list

--- src/components/favourites-list/MoviePreview.js
+++ src/components/favourites-list/MoviePreview.js
@@ -4,36 +4,51 @@ import Button from '../elements/Button'
 import api from "../../api/localStorage"
 
 class MoviePreview extends Component {
-  // TODO: Get movie & update it on unfavourite event reactive via Redux.
+  state = {
+    showPreview: true
+  }
   removeFromFavourite = () => {
+    this.setState({ showPreview: false })
     api.removeFavourite(this.props.movie.id)
+  }
+  returnFavourite = () => {
+    this.setState({ showPreview: true })
+    api.setFavourite(this.props.movie)
   }
   render() {
     const { movie } = this.props
     return (
-      <div className="movie-preview">
-        <img src={getPicture(movie)}
-             alt="Not found"
-             className="movie-preview__image"
-        />
-        <div className="movie-preview__content">
-          <div className="movie-preview__header">
-            <div className="movie-preview__title">
-              {movie.original_title}
+      this.state.showPreview
+        ? <div className="movie-preview">
+          <img src={getPicture(movie)}
+               alt="Not found"
+               className="movie-preview__image"
+          />
+          <div className="movie-preview__content">
+            <div className="movie-preview__header">
+              <div className="movie-preview__title">
+                {movie.original_title}
+              </div>
+              <div className="movie-preview__button-container">
+                <Button onClick={this.removeFromFavourite}
+                        text="Unfavourite"
+                        white
+                        className="movie-preview__button"
+                />
+              </div>
             </div>
-            <div className="movie-preview__button-container">
-              <Button onClick={this.removeFromFavourite}
-                      text="Unfavourite"
-                      white
-                      className="movie-preview__button"
-              />
+            <div className="movie-preview__description">
+              {movie.overview}
             </div>
-          </div>
-          <div className="movie-preview__description">
-            {movie.overview}
           </div>
         </div>
-      </div>
+        : <div className="movie-preview">
+          <Button onClick={this.returnFavourite}
+                  text="Return"
+                  white
+                  className="movie-preview__button"
+          />
+        </div>
     )
   }
 }
